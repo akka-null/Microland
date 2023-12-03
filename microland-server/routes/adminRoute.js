9c2c8efd6fcbe39b6e2bd52c4138b4e70f62c79c
akka <akkabeztout@gmail.com>
2023-12-03 01:23:52 +0100

    Delete microland-server directory

--- microland-server/routes/adminRoute.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-const router = Router();
-import adminController from "../controllers/adminController.js";
-
-router.get("/", adminController.getDashBoard);
-router.post("/", adminController.addProd);
-
-export default router;
--- microland-server/routes/shopRoute.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const router = Router();
-import shopController from "../controllers/shopController.js";
-
-router.get("/products", shopController.getProducts);
-router.get("/product/:prodId", shopController.getProductById);
-
-export default router;
